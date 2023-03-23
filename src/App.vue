1667eb50e58b632552017a8c9c31fd0caaae3b09
Mednabilmessaoudi <messaoudi.nabil.17@gmail.com>
2023-03-23 14:10:11 +0100

    ask

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <div class="container">
+      <transition name="fade" appear mode="out-in">
+        <component
+          :is="screens[position]"
+          :question="question"
+          :result="result"
+          @goto="handleGoTo"
+          @question="handleQuestion"
+          @showResult="showResult"
+          @startOver="startOver"
+        />
+      </transition>
+    </div>
+  </template>
+  
+  <script>
+  import appInitial from "./components/DisplayInitial.vue";
+  import appConfirm from "./components/NeedConfirm.vue";
+  import appResults from "./components/DisplayResults.vue";
+  export default {
+    components: {
+      appInitial,
+      appConfirm,
+      appResults,
+    },
+    data() {
+      return {
+        list: [
+          "Yes",
+          "No",
+          "Maybe",
+          "Not sure..try again",
+          "Ask a friend",
+          "Call the police",
+        ],
+        screens: ["appInitial", "appConfirm", "appResults"],
+        position: 0,
+        question: "",
+        result: "",
+      };
+    },
+    methods: {
+      handleGoTo(position) {
+        this.position = position;
+      },
+      handleQuestion(question) {
+        this.question = question;
+      },
+      getRandomValue() {
+        return this.list[Math.floor(Math.random() * this.list.length)];
+      },
+      generateResult() {
+        let rand = this.getRandomValue();
+        if(this.result !== ''){
+            while (rand === this.result){
+              rand = this.getRandomValue();
+            }
+        }
+        this.result = rand;
+      },
+      showResult() {
+        this.generateResult();
+      },
+      startOver(){
+          this.position = 0;
+          this.question = "";
+          this.result = "";
+      }
+    },
+  };
+  </script>
+  
+  <style>
+  @import "./assets/style.css";
+  .fade-enter-from {
+    opacity: 0;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  .fade-enter-active {
+    transition: 0.5s;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+  .fade-leave-from {
+    opacity: 1;
+  }
+  .fade-leave-active {
+    transition: 0.5s;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
